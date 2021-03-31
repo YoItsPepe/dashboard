0fb71980632d99c23670f1fdd59dce76bf298709
YoItsPepe <tradeu8@gmail.com>
2021-03-30 23:46:20 -0400

    Updates

---
+++ precache-manifest.3a4f165a8b2caf7277f2b0065ad10c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b745a3b6662097139eaa4afad587760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c0e00351cbc0ffb632b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2333aeb98047e6cacdb6",
+    "url": "/static/js/2.4bf358ac.chunk.js"
+  },
+  {
+    "revision": "01e78a743a56753e6f8d36e4b90a9363",
+    "url": "/static/js/2.4bf358ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0e00351cbc0ffb632b",
+    "url": "/static/js/main.ca8f3c52.chunk.js"
+  },
+  {
+    "revision": "f364fb7b2babcf1e4921",
+    "url": "/static/js/runtime-main.fb7dcf2a.js"
+  }
+]);
