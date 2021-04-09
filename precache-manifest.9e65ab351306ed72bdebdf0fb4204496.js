9c82da541896e75c6b64e66f288532b0d20769a4
DeFiGuy <governancesale@protonmail.com>
2021-04-09 19:04:50 -0400

    Updates

---
+++ precache-manifest.9e65ab351306ed72bdebdf0fb4204496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88215d2bc9ac4129c6242a0e2009265b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56f3507ba63327827b4b",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c6a31fa71a290e4d5cde",
+    "url": "/static/js/2.ad84eb8f.chunk.js"
+  },
+  {
+    "revision": "01e78a743a56753e6f8d36e4b90a9363",
+    "url": "/static/js/2.ad84eb8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f3507ba63327827b4b",
+    "url": "/static/js/main.11af7e4b.chunk.js"
+  },
+  {
+    "revision": "f364fb7b2babcf1e4921",
+    "url": "/static/js/runtime-main.fb7dcf2a.js"
+  }
+]);
